42ed8da2c55b1ad4a0e9ef1a90a5d134e3b0c3e2
Enrico Speranza <enrico.speranza@gmail.com>
2025-02-08 10:46:47 +0000

    First stub using go-staticmap

--- go.mod
+++ go.mod
@@ -2,4 +2,22 @@ module atcas2staticmap
 
 go 1.23.4
 
-require github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1
+	github.com/golang/geo v0.0.0-20230421003525-6adc56603217
+)
+
+require (
+	github.com/flopp/go-coordsparser v0.0.0-20240403152942-4891dc40d0a7 // indirect
+	github.com/mazznoer/csscolorparser v0.1.5 // indirect
+	github.com/tkrajina/gpxgo v1.4.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
+
+require (
+	github.com/flopp/go-staticmaps v0.0.0-20250206111937-47d062eaabce
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,14 +5,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
+
+	"image/color"
 
 	"github.com/gocarina/gocsv"
+
+	sm "github.com/flopp/go-staticmaps"
+	"github.com/fogleman/gg"
+	"github.com/golang/geo/s2"
 )
 
 type Record struct {
 	TIME string `csv:"TIME"`
 	LAT  string `csv:"LAT"`
-	LON string `csv:"LON"`
+	LON  string `csv:"LON"`
+}
+
+func StringToFloat64(data string) float64 {
+	if s, err := strconv.ParseFloat(data, 64); err == nil {
+		return s
+	} else {
+		return 0.0
+	}
 }
 
 func main() {
@@ -23,14 +39,46 @@ func main() {
 	}
 	defer file.Close()
 
+	ctx := sm.NewContext()
+	ctx.SetSize(1920, 1080)
+
 	// Read the CSV file into a slice of Record structs
 	var records []Record
 	if err := gocsv.UnmarshalFile(file, &records); err != nil {
 		panic(err)
 	}
 
+	//Read the csv file and len
+	v := reflect.ValueOf(records)
+	i := 0
+	path := make([]s2.LatLng, 0, 2)
+
 	// Print the records
 	for _, record := range records {
+		if i == 0 {
+			first := sm.NewMarker(s2.LatLngFromDegrees(StringToFloat64(record.LAT), StringToFloat64(record.LON)), color.RGBA{255, 0, 0, 255}, 16.0)
+			ctx.AddObject(first)
+			path = append(path, first.Position)
+		}
+		if i == v.NumField() {
+			last := sm.NewMarker(s2.LatLngFromDegrees(StringToFloat64(record.LAT), StringToFloat64(record.LON), color.RGBA{0, 0, 255, 255}, 16.0)
+			ctx.AddObject(last)
+			path = append(path, last.Position)
+		}
 		fmt.Printf("TIME: %s, LAT: %s, LON: %s\n", record.TIME, record.LAT, record.LON)
+		// Add all others plots
+		path = append(path, s2.LatLngFromDegrees(StringToFloat64(record.LAT), StringToFloat64(record.LON)))
+		i = i + 1
+	}
+	// Total path
+	ctx.AddObject(sm.NewPath(path, color.RGBA{0, 255, 0, 255}, 4.0))
+
+	img, err := ctx.Render()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := gg.SavePNG("1136.png", img); err != nil {
+		panic(err)
 	}
 }
