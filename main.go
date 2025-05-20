ac212d89239eee3e5ce54513291441fd97b075e9
Enrico Speranza <enrico.speranza@gmail.com>
2025-05-20 22:52:47 +0200

    Version 1.0.0

--- main.go
+++ main.go
@@ -40,6 +40,23 @@ func IntToString(n int) string {
 	return strconv.Itoa(n)
 }
 
+// NOTE: this isn't multi-Unicode-codepoint aware, like specifying skintone or
+//
+//	gender of an emoji: https://unicode.org/emoji/charts/full-emoji-modifiers.html
+func substr(input string, start int, length int) string {
+	asRunes := []rune(input)
+
+	if start >= len(asRunes) {
+		return ""
+	}
+
+	if start+length > len(asRunes) {
+		length = len(asRunes) - start
+	}
+
+	return string(asRunes[start : start+length])
+}
+
 // TextMarker is an MapObject that displays a text and has a pointy tip:
 //
 //	+------------+
@@ -114,20 +131,26 @@ func (s *TextMarker) Draw(gc *gg.Context, trans *sm.Transformer) {
 	gc.DrawString(s.Text, x-s.TextWidth*0.5, y-s.TipSize-4.0)
 }
 
-func CreateTracksImage(csvPaths []string, outputImage string) error {
+type TrackInput struct {
+	FilePath string
+	Color    color.RGBA
+}
+
+// Modifica la funzione per accettare []TrackInput invece di []string
+func CreateTracksImage(tracks []TrackInput, outputImage string) error {
 	ctx := sm.NewContext()
 	ctx.SetSize(1920, 1080)
 
-	for _, csvPath := range csvPaths {
-		file, err := os.Open(csvPath)
+	for _, track := range tracks {
+		file, err := os.Open(track.FilePath)
 		if err != nil {
-			return fmt.Errorf("errore apertura file %s: %w", csvPath, err)
+			return fmt.Errorf("errore apertura file %s: %w", track.FilePath, err)
 		}
 
 		var records []Record
 		if err := gocsv.UnmarshalFile(file, &records); err != nil {
 			file.Close()
-			return fmt.Errorf("errore parsing file %s: %w", csvPath, err)
+			return fmt.Errorf("errore parsing file %s: %w", track.FilePath, err)
 		}
 		file.Close()
 
@@ -138,28 +161,50 @@ func CreateTracksImage(csvPaths []string, outputImage string) error {
 		v := len(records) - 1
 		path := make([]s2.LatLng, 0, len(records)+2)
 
+		//Flight
+		var StrToShow string
+		flight := substr(track.FilePath, 0, 4)
+		if flight == "1136" {
+			StrToShow = "DC9 ITAVIA IH870 (Bologna-Palermo) 1136"
+		} else if flight == "1133" {
+			StrToShow = "Fokker 28 ITAVIA IH779 (Bergamo-Roma) 1133"
+		} else if flight == "1132" {
+			StrToShow = "ATI BM300 (Trieste-Roma) 1132"
+		} else if flight == "0225" {
+			StrToShow = "B727 I-DIRU (Pezzopane) Alitalia AZ865 (Tunisi-Fiumicino) 0225"
+		} else if flight == "0226" {
+			StrToShow = "Bea Tours KT881 (Malta-Londra) 0226"
+		} else if flight == "1235" {
+			StrToShow = "Boing 720 Air Malta KM153 (Londra-Malta) 1235"
+		} else if flight == "4200" {
+			StrToShow = "TF-104G (20-4 MM54230) Bergamini/Moretti (Intermedia 8)"
+		} else {
+			StrToShow = "Unknown"
+		}
+
 		for i, record := range records {
 			lat := StringToFloat64(record.LAT)
 			lon := StringToFloat64(record.LON)
 			pos := s2.LatLngFromDegrees(lat, lon)
 
 			if i == 0 {
-				FirstText := NewTextMarker(pos, record.TIME)
+				FirstText := NewTextMarker(pos, record.TIME+" "+StrToShow)
 				ctx.AddObject(FirstText)
-				first := sm.NewMarker(pos, color.RGBA{0, 255, 0, 255}, 4.0)
+				first := sm.NewMarker(pos, color.RGBA{0, 255, 0, 255}, 8.0)
 				ctx.AddObject(first)
 				path = append(path, first.Position)
 			}
 			if i == v {
 				LastText := NewTextMarker(pos, record.TIME)
 				ctx.AddObject(LastText)
-				last := sm.NewMarker(pos, color.RGBA{255, 0, 0, 255}, 4.0)
+				last := sm.NewMarker(pos, color.RGBA{255, 0, 0, 255}, 8.0)
 				ctx.AddObject(last)
 				path = append(path, last.Position)
 			}
 			path = append(path, pos)
 		}
-		ctx.AddObject(sm.NewPath(path, color.RGBA{0, 255, 0, 255}, 4.0)) // semi-trasparente per più tracce
+		// Usa il colore specificato per la traccia
+		ctx.AddObject(sm.NewPath(path, track.Color, 4.0))
 	}
 
 	img, err := ctx.Render()
@@ -173,13 +218,40 @@ func CreateTracksImage(csvPaths []string, outputImage string) error {
 	return nil
 }
 
-func main() {
-	csvFiles := []string{
-		"1136-varie.csv",
-		"1133-varie.csv",
-		// aggiungi altri file qui
+func HexToRGBA(hex string) color.RGBA {
+	hex = strings.TrimPrefix(hex, "#")
+	var r, g, b uint8 = 0, 0, 0
+	if len(hex) == 6 {
+		ri, _ := strconv.ParseUint(hex[0:2], 16, 8)
+		gi, _ := strconv.ParseUint(hex[2:4], 16, 8)
+		bi, _ := strconv.ParseUint(hex[4:6], 16, 8)
+		r, g, b = uint8(ri), uint8(gi), uint8(bi)
 	}
-	err := CreateTracksImage(csvFiles, "tutte_le_tracce.png")
+	return color.RGBA{r, g, b, 255}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Uso: main.go file1.csv:#RRGGBB file2.csv:#RRGGBB ...")
+		return
+	}
+	var tracks []TrackInput
+	for _, arg := range os.Args[1:] {
+		parts := strings.Split(arg, ":")
+		if len(parts) != 2 {
+			fmt.Printf("Argomento non valido: %s. Usa file.csv:#RRGGBB\n", arg)
+			continue
+		}
+		tracks = append(tracks, TrackInput{
+			FilePath: parts[0],
+			Color:    HexToRGBA(parts[1]),
+		})
+	}
+	if len(tracks) == 0 {
+		fmt.Println("Nessuna traccia valida fornita.")
+		return
+	}
+	err := CreateTracksImage(tracks, "tutte_le_tracce.png")
 	if err != nil {
 		panic(err)
 	}
---
+++ runme.bat
@@ -0,0 +1,1 @@
+.\atcas2staticmap.exe 1136-varie.csv:#FF0000 1133-varie.csv:#00FF00 1132-varie.csv:#0000FF 0225-varie.csv:#00FF00 0226-varie.csv:#00FF00 1235-varie.csv:#00FF00
